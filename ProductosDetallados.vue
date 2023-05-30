abe7cfdf9ea17cf8b733cc242eff4132510dc460
vicent86 <79800812+vicent86@users.noreply.github.com>
2023-05-30 21:17:24 +0200

    Add files via upload
    Modificat metode

--- ProductosDetallados.vue
+++ ProductosDetallados.vue
@@ -1,94 +1,94 @@
 <template>
-    <div id="page-wrap" v-if="producto">
-        <div id="img-wrap">
-            <img v-bind:src="producto.imagen" />
-        </div>
-        <div id="products-details" class="q-pa-md q-gutter-sm">
-            <h1>{{ producto.nombre }}</h1>
-            <h3 id="price">{{ producto.precio }}</h3>
-            <p>Puntuación Media: {{ producto.PuntacionMedia }}</p>
-            <q-btn  color ="black" label="Añadir al carro" class="add-to-cart" 
-              v-on:click="addProduct"
-              
-            />
-            <h4>Descripción</h4>
-            <p>{{ producto.descripcion }}</p>
-        </div>
+  <div id="page-wrap" v-if="producto">
+    <div id="img-wrap">
+      <img v-bind:src="producto.imagen" />
     </div>
-    <NoseEncuentra v-else />
+    <div id="products-details">
+      <h1>{{ producto.nombre }}</h1>
+      <h3 id="price">{{ producto.precio }}</h3>
+      <p>Puntuación Media: {{ producto.PuntacionMedia }}</p>
+      <button class="add-to-cart" v-on:click="addProduct">
+        Añadir al carrito
+      </button>
+      <h4>Descripción</h4>
+      <p>{{ producto.descripcion }}</p>
+    </div>
+  </div>
+  <NoseEncuentra v-else />
 </template>
-  
+
 <script>
-  import axios from 'axios';
-  import NoseEncuentra from '../views/NoSeEncuentra.vue';
-  import { productos } from '../data'; // Importa la variable productos
-  
-  export default {
-    name: "DetallesProductos",
-    components: {
-      NoseEncuentra,
-    },
-    async created() {
-      const resultado = await axios.get(`/api/productos/${this.$route.params.id}`);
-      const producto = resultado.data;
-      this.producto = producto;
-    },
-    computed: {
-      producto() {
-        const id = this.$route.params.id;
-        if (id) {
-          const producto = productos.find(producto => producto.id === id);
-          return producto || {};
+import axios from 'axios';
+import NoseEncuentra from '../views/NoSeEncuentra.vue';
+
+export default {
+  name: 'DetallesProductos',
+  components: {
+    NoseEncuentra,
+  },
+  data() {
+    return {
+      producto: null,
+    };
+  },
+  async created() {
+    const productoId = this.$route.params.productoId;
+    let productoData = null;
+    try {
+      const response = await axios.get(`/api/productos/${productoId}`);
+      productoData = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+    this.producto = productoData;
+  },
+  methods: {
+    async addProduct() {
+      const usuarioId = this.$route.params.usuarioId;
+      const productoId = this.$route.params.productoId;
+     
+      try {
+        const result = await axios.post(`/api/usuarios/${usuarioId}/carrito`, {
+          productoId: productoId,
+          usuarioId: usuarioId,
+         
+        });
+        if (result.status === 200) {
+          this.$router.push('/carrito');
+          alert('Producto añadido al carrito');
         } else {
-          return {};
+          alert('Producto no encontrado');
         }
-      },
-    },
-    methods: {
-      async addProduct() {
-        const usuarioId = this.$route.params.usuarioId;
-        const productoId = this.$route.params.productoId;
-        try {
-          const response = await axios.post( `/api/usuarios/${usuarioId}/carrito`, {
-            productoId: productoId,
-          });
-          console.log(response);
-          if (response.status === 200) {
-            this.$router.push('/carrito');
-            alert('Producto añadido al carrito');
-          } else {
-            alert('Producto no encontrado');
-            
-          }
-        } catch (error) {
-          console.error(error.response);
-          if (error.response && error.response.data && error.response.data.error) {
-            alert('Error al agregar el producto: ' + error.response.data.error);
-          } else {
-            alert('Error al agregar el producto');
-          }
-          
+      } catch (error) {
+        if (error.response && error.response.data && error.response.data.error) {
+          alert('Error al agregar el producto: ' + error.response.data.error);
+        } else {
+          alert('Error al agregar el producto');
         }
       }
-
     },
- };
+  },
+};
 </script>
-  
+
 <style scoped>
 h1 {
-color: blue;
+  color: blue;
 }
 
 #add-to-cart {
-width: 100%;
+  width: 100%;
 }
 
 #price {
-position: absolute;
-top: 74px;
-right: 16px;
+  position: absolute;
+  top: 74px;
+  right: 16px;
 }
 
+button {
+  width: 100%;
+  background: black;
+  color: white;
+}
 </style>
-  
