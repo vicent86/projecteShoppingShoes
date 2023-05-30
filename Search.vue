005c893e18db861e0911efe04ee88e47e6ea302b
vicent86 <79800812+vicent86@users.noreply.github.com>
2023-05-30 13:19:40 +0200

    barra de busqueda V2

---
+++ Search.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="barra-busqueda">
+      <q-input
+        outlined
+        dense
+        v-model="busqueda"
+        placeholder="Buscar..."
+        clearable
+        @clear="limpiarBusqueda"
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </div>
+</template>
+  
+<script>
+  export default {
+    name: 'MiComponente',
+    data() {
+      return {
+        busqueda: ''
+      }
+    },
+    methods: {
+      limpiarBusqueda() {
+        this.busqueda = '';
+      }
+    }
+  }
+</script>
+  
+<style scoped>
+  .barra-busqueda {
+    width: 300px; /* Ajusta el ancho según tus necesidades */
+  }
+</style>
+  
